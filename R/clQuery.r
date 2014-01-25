e9abd0f1db3055b59586a33815610b17e895ad7e
leeper <thosjleeper@gmail.com>
2014-01-25 18:04:45 +0100

    tweaks to clquery

--- R/clQuery.r
+++ R/clQuery.r
@@ -1,18 +1,24 @@
-clquery <- function(type, subtype = NULL, query = NULL, fmt = 'xml', ...){
+clquery <- function(type, set = NULL, query = NULL, fmt = 'xml', ...){
 	# API workhorse query function
-	if(!type %in% c('colors', 'palettes', 'patterns', 'lovers', 'stats'))
+	if(!type %in% c('color', 'colors',
+                    'palette', 'palettes',
+                    'pattern', 'patterns',
+                    'lover', 'lovers',
+                    'stats'))
         warning("API type not recognized")
 	url <- paste('http://www.colourlovers.com/api/',type,sep="")
-    if(!is.null(subtype))
-        url <- paste(url,subtype,sep="/")
-	if(!is.null(query))
-		url <- paste(url,query,sep="/")
+    if(!is.null(set))
+    url <- paste(url,set,sep="/")
     
-    if(is.null(fmt) || !fmt %in% c('xml','json'))
+    if(is.null(fmt) || !fmt %in% c('xml','json')){
         warning('fmt is missing or incorrect; xml used by default')
-
-    # handle parameters
+        fmt <- 'xml'
+    }
     url <- paste(url,'?format=',fmt,sep='')
+    # handle parameters
+    query <- paste(names(query),query,sep='=',collapse='&')
+    url <- paste(url,query,sep="&")
+    
     
     #response <- getURL(url, followlocation = 1L, ...)
     urlcon <- url(url)
@@ -21,7 +27,7 @@ clquery <- function(type, subtype = NULL, query = NULL, fmt = 'xml', ...){
     
     # handle json or xml
     if(fmt == 'xml'){
-        out <- xmlToList(response)
+        out <- xmlToList(response, addAttributes=FALSE)
     } else if(fmt=='json'){
         out <- fromJSON(response, simplify=FALSE)
     } else
