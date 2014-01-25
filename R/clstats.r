edc92c83a70c6297f5613e3326ea7730691fcd41
leeper <thosjleeper@gmail.com>
2014-01-25 15:55:25 +0100

    updated clstats

--- R/clstats.r
+++ R/clstats.r
@@ -1,7 +1,7 @@
 clstats <- function(type, fmt='xml', ...){
     if(!type %in% c('colors', 'palettes', 'patterns', 'lovers'))
         stop("type must be 'colors', 'palettes', 'patterns', or 'lovers'")
-    out <- clquery('stats', type, fmt=fmt, ...)
+    out <- clquery('stats', type, fmt=fmt)
     class(out) <- c('clstats',class(out))
     attr(out,'clstat_type') <- type
     return(out)
