d7975ea064c34160c1a3f4b262da0ccb92f2900f
leeper <thosjleeper@gmail.com>
2014-01-25 22:45:24 +0100

    minor change to swatch

--- R/swatch.r
+++ R/swatch.r
@@ -5,26 +5,33 @@ swatch <- function(x, ...){
     s2 <- inherits(x, 'clcolors') | inherits(x, 'clpalettes') | inherits(x, 'clpatterns')
     if(s1) {
         u <- x$imageUrl
+        if(inherits(x,'clpalette'))
+            par(mar=c(2,1,2,1))
+        else
+            par(mar=c(1,1,2,1))    
     } else if(s2){
-        par(ask=TRUE)
+        if(inherits(x,'clpalette'))
+            par(mar=c(2,1,2,1), ask=TRUE)
+        else
+            par(mar=c(1,1,2,1), ask=TRUE)
         sapply(x, plot)
         return(invisible(x))
     }
-    download.file(u, tmp <- tempfile(), mode='wb')
-    if(inherits(x, 'clcolors'))
-        m <- paste('Color #',x$id,sep='')
+    tmp <- tempfile()
+    download.file(u, tmp, quiet=TRUE, mode='wb')
+    if(inherits(x, 'clcolor'))
+        m <- paste('Color #',x$hex,sep='')
     else if(inherits(x, 'clpalette'))
         m <- paste('Palette #',x$id,sep='')
     else if(inherits(x, 'clpattern'))
         m <- paste('Pattern #',x$id,sep='')
     else
         m <- ''
-    par(mar=c(1,1,2,1))
     plot(1:2, type='n', bty='n', xlab='', ylab='', xaxt='n', yaxt='n', fg='white', main=m)
     rasterImage(readPNG(tmp),1,1,2,2)
     if(inherits(x, 'clpalette'))
         axis(1, seq(1,2,length.out=5), unlist(x$colors))
-    unlink(tmp)
+    file.remove(tmp)
     return(invisible(x))
 }
 
