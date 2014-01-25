a23a5eeb1e5fd9c6aaafad6660f07c263235cf72
leeper <thosjleeper@gmail.com>
2014-01-25 17:31:48 +0100

    add cllovers functions

--- R/cllovers.r
+++ R/cllovers.r
@@ -1,18 +1,66 @@
-cllover <- function(user, fmt='xml', ...){
-    # request a single lover
-    out <- clquery('lover', user, fmt=fmt, ...)
+cllover <- function(user, comments = FALSE, fmt = 'xml'){
+    # request a single lover/user
+    out <- list(lover=clquery('lover', user, query=list(comments=as.numeric(comments)), fmt=fmt)[[1]])
     class(out) <- c('cllover',class(out))
     return(out)
 }
 
-cllovers <- function(type=NULL, query = NULL, fmt='xml', ...){
+cllovers <- function(set = NULL, ..., fmt = 'xml'){
     # request multiple lovers
-    if(!is.null(type) && !type %in% c('new', 'top'))
+    if(!is.null(set) && !set %in% c('new', 'top'))
         stop("type must be 'new' or 'top'")
-    if(type=='random') # no query parameters allowed
-        out <- clquery('lovers', type, fmt=fmt, ...)
-    else
-        out <- clquery('lovers', type, fmt=fmt, ...)
+    query <- list(...)
+    query <- query[!sapply(query, is.null)]
+    query <- query[-which(names(query)=='')]
+    allowed <- c('orderCol','sortBy','numResults','resultOffset')
+    query[which(!names(query) %in% allowed)] <- NULL
+    n <- names(query)
+    if('orderCol' %in% n){
+        ord <- c('dateCreated','score','name','numVotes','numViews')
+        if(!query$orderCol %in% ord){
+            query$orderCol <- NULL
+            warning("orderCol not recognized")
+        }
+    }
+    if('sortBy' %in% n){
+        if(!query$sortBy %in% c('ASC','DSC')){
+            query$sortBy <- NULL
+            warning("sortBy not recognized")
+        }
+    }
+    if('numResults' %in% n){
+        if(query$numResults > 100 | query$numResults < 1)
+            query$numResults <- 20
+    }
+    if('resultOffset' %in% n){
+        if(query$resultOffset < 1)
+            query$resultOffset <- 1
+    }            
+    
+    out <- clquery('lovers', set, query=NULL, fmt=fmt)
+    names(out) <- rep('lover',length(out))
     class(out) <- c('cllover',class(out))
     return(out)
+}
+
+print.cllover <- function(x,...) {
+    onelover <- function(z){
+        cat('Lover username:     ', z$userName[[1]],'\n')
+        cat('Registered:         ', z$dateRegistered,'\n')
+        cat('Last active:        ', z$dateLastActive,'\n')
+        cat('Rating:             ', z$rating,'\n')
+        cat('Location:           ', z$location[[1]],'\n')
+        cat('Colors:             ', z$numColors,'\n')
+        cat('Palettes:           ', z$numPalettes,'\n')
+        cat('Patterns:           ', z$numPatterns,'\n')
+        cat('Comments made:      ', z$numCommentsMade,'\n')
+        cat('Lovers:             ', z$numLovers,'\n')
+        cat('Comments on profile:', z$numCommentsOnProfile,'\n')
+        cat('URL:                ', z$url[[1]],'\n')
+        cat('API URL:            ', z$apiUrl[[1]],'\n')
+        cat('\n')
+    }
+    for(i in which(names(x)=='lover'))
+        onelover(x[[i]])
+    invisible(x)
 }
