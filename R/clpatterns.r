5cf66a444526778c22769e727835b23c215ee7fd
leeper <thosjleeper@gmail.com>
2014-01-25 18:04:27 +0100

    continue work on clpatterns

--- R/clpatterns.r
+++ R/clpatterns.r
@@ -1,18 +1,89 @@
-clpattern <- function(id, fmt='xml', ...){
+clpattern <- function(id, fmt='xml'){
     # request a single pattern
-    out <- clquery('pattern', id, fmt=fmt, ...)
+    out <- clquery('pattern', id, fmt=fmt)
     class(out) <- c('clpattern',class(out))
     return(out)
 }
 
-clpatterns <- function(type=NULL, query = NULL, fmt='xml', ...){
+clpatterns <- function(set = NULL, ..., fmt='xml'){
     # request multiple patterns
-    if(!is.null(type) && !type %in% c('new', 'top', 'random'))
-        stop("type must be 'new', 'top', or 'random'")
-    if(type=='random') # no query parameters allowed
-        out <- clquery('patterns', type, fmt=fmt, ...)
-    else
-        out <- clquery('patterns', type, fmt=fmt, ...)
+    if(!is.null(set) && !set %in% c('new', 'top', 'random'))
+        stop("set must be 'new', 'top', or 'random', or NULL")
+    if(set=='random') {
+        out <- clquery('patterns', set, query=NULL, fmt=fmt)
+        if(!is.null(query))
+            warning("query parameters ignored for 'random' patterns")
+    } else {        
+        query <- list(...)
+        query <- query[!sapply(query, is.null)]
+        query <- query[-which(names(query)=='')]
+        allowed <- c('lover','hueOption','hex','hex_logic','keywords',
+                     'keywordsExact','orderCol','sortBy','numResults',
+                     'resultOffset')
+        query[which(!names(query) %in% allowed)] <- NULL
+        n <- names(query)
+        if('hueOption' %in% n){
+            cols <- c('red','orange','yellow','green',
+                      'aqua','blue','violet','fuchsia')
+            query$hueOption <- query$hueOption[query$hueOption %in% cols]
+            query$hueOption <- paste(query$hueOption,sep=',')
+        }
+        if('hex' %in% n){
+            if(length(query$hex)>5){
+                warning(paste(length(query$hex),
+                        'hex values supplied, only first five used.'))
+                query$hex <- query$hex[1:5]
+            }
+            query$hex <- paste(query$hex,sep=',')
+        }
+        if('hex_logic' %in% n){
+            query$hex_logic <- toupper(query$hex_logic)
+            if(!query$hex_logic %in% c('AND','OR')){
+                warning('hex_logic must be AND | OR; AND used by default')
+                query$hex_logic <- 'AND'
+            }
+        }
+        if('keywords' %in% n){
+            query$keywords <- gsub(' ', '+', query$keywords)
+        }
+        if('keywordExact' %in% n){
+            if(query$keywordExact==TRUE)
+                query$keywordExact <- 1
+            if(query$keywordExact==FALSE)
+                query$keywordExact <- 0
+            if(!query$keywordExact %in% 0:1){
+                warning('keywordExact must be 0 (FALSE) | 1 (TRUE); 0 used by default')
+                query$keywordExact <- 0
+            }
+        }
+        if('orderCol' %in% n){
+            ord <- c('dateCreated','score','name','numVotes','numViews')
+            if(!query$orderCol %in% ord){
+                query$orderCol <- NULL
+                warning("orderCol not recognized")
+            }
+        }
+        if('sortBy' %in% n){
+            if(!query$sortBy %in% c('ASC','DSC')){
+                query$sortBy <- NULL
+                warning("sortBy not recognized")
+            }
+        }
+        if('numResults' %in% n){
+            if(query$numResults > 100 | query$numResults < 1)
+                query$numResults <- 20
+        }
+        if('resultOffset' %in% n){
+            if(query$resultOffset < 1)
+                query$resultOffset <- 1
+        }
+        out <- clquery('patterns', set, query=query, fmt=fmt)
+    }
     class(out) <- c('clpattern',class(out))
     return(out)
+}
+
+
+print.clpattern <- function(x,...) {
+    x
 }
