1c4f8d36f1ed2a07f0408ec5fdf45d637139d560
leeper <thosjleeper@gmail.com>
2014-01-25 20:30:39 +0100

    change clstats args

--- R/clstats.r
+++ R/clstats.r
@@ -1,4 +1,4 @@
-clstats <- function(type, fmt='xml', ...){
+clstats <- function(type, fmt='xml'){
     if(!type %in% c('colors', 'palettes', 'patterns', 'lovers'))
         stop("type must be 'colors', 'palettes', 'patterns', or 'lovers'")
     out <- clquery('stats', type, fmt=fmt)
