ced5d20a39605b7d645474c5e9c1e6f5b9890b3e
Andrew Heiss <andrew@andrewheiss.com>
2019-04-27 23:50:20 -0600

    Add tests for JSON class

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -16,6 +16,20 @@ with_mock_api({
     })
 })
 
+with_mock_api({
+    test_that("Colors work with JSON", {
+        r <- clcolors('random', fmt = "json")
+        expect_true(inherits(r, "clcolor"), label = "clcolors class correct")
+        expect_true(is.list(swatch(r)), label = "clcolors swatch is list")
+        expect_true(length(swatch(r)[[1]]) == 1, label = "clcolors swatch has one color")
+        
+        c1 <- clcolor('6B4106', fmt = "json")
+        expect_true(inherits(c1, "clcolor"), label = "clcolor class correct")
+        expect_true(is.list(swatch(c1)), label = "clcolors swatch is list")
+        
+        c2 <- clcolor(hsv(.5,.5,.5), fmt = "json")
+        expect_true(inherits(c2, "clcolor"), label = "clcolor parsing works")
+    })
 })
 
 context("Palettes")
