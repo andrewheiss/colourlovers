addf8457a6dec32e7c2ccc72aa62824a55b366c4
Andrew Heiss <andrew@andrewheiss.com>
2019-04-30 12:12:58 -0600

    Use mock API

--- tests/testthat/test-statistics.R
+++ tests/testthat/test-statistics.R
@@ -1,20 +1,24 @@
 context("Statistics")
 
-test_that("Statistics work", {
-  s <- clstats('colors')
-  expect_true(inherits(s, "clstats"), 
-              label = "clstats class correct")
-  expect_equal(attr(s, "clstat_type"), "colors", 
-              label = "clstat_type class correct")
-  expect_gt(s, 1, label = "number is greater than 1")
+with_mock_api({
+  test_that("Statistics work", {
+    s <- clstats('colors')
+    expect_true(inherits(s, "clstats"), 
+                label = "clstats class correct")
+    expect_equal(attr(s, "clstat_type"), "colors", 
+                 label = "clstat_type class correct")
+    expect_gt(s, 1, label = "number is greater than 1")
+  })
 })
 
-test_that("Printing works", {
-  s <- clstats('colors')
-  s_output <- capture.output(s)
-  
-  expect_equal(length(s_output), 1,
-               label = "print output is correct length")
-  expect_match(s_output, "Total colors:",
-               label = "correct label included")
+with_mock_api({
+  test_that("Printing works", {
+    s <- clstats('colors')
+    s_output <- capture.output(s)
+    
+    expect_equal(length(s_output), 1,
+                 label = "print output is correct length")
+    expect_match(s_output, "Total colors:",
+                 label = "correct label included")
+  })
 })
