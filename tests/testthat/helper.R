0310a500ab89924e720b47213517af011ae752e6
Andrew Heiss <andrew@andrewheiss.com>
2020-12-08 20:11:28 -0500

    Use vdiffr conditionally

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,4 +1,10 @@
 library(httptest)
-library(vdiffr)
-
 httptest::.mockPaths("../")
+
+# Custom expect_doppelganger() because vidffr struggles to install on M1 Macs
+# Code via Lionel Henry
+# https://github.com/lionel-/ggstance/commit/eac216f6
+expect_doppelganger <- function(title, fig, path = NULL, ...) { 
+  testthat::skip_if_not_installed("vdiffr") 
+  vdiffr::expect_doppelganger(title, fig, path = path, ...) 
+}
--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -7,22 +7,22 @@ with_mock_api({
     testthat::skip_on_cran()
 
     disp_single_color <- function() plot(clcolor(rgb(0, 0, 1)))
-    vdiffr::expect_doppelganger("single color plot", disp_single_color)
+    expect_doppelganger("single color plot", disp_single_color)
     
     disp_multiple_colors <- function() plot(clcolors(set = "top", 
                                                      lover = "Skyblue2u", 
                                                      numResults = 1), 
                                             ask = FALSE)
-    vdiffr::expect_doppelganger("multiple color plots", disp_multiple_colors)
+    expect_doppelganger("multiple color plots", disp_multiple_colors)
     
     disp_pie_color <- function()  plot(clcolor(rgb(0, 0, 1)), type = "pie")
-    vdiffr::expect_doppelganger("single color pie chart", disp_pie_color)
+    expect_doppelganger("single color pie chart", disp_pie_color)
     
     disp_multiple_pie_colors <- function() plot(clcolors(set = "top", 
                                                          lover = "Skyblue2u", 
                                                          numResults = 1), 
                                                 ask = FALSE, type = "pie")
-    vdiffr::expect_doppelganger("multiple color pie charts", disp_multiple_pie_colors)
+    expect_doppelganger("multiple color pie charts", disp_multiple_pie_colors)
   })
 })
 
@@ -31,25 +31,25 @@ with_mock_api({
     testthat::skip_on_cran()
     
     disp_single_palette <- function() plot(clpalette("4643792"))
-    vdiffr::expect_doppelganger("single palette plot", disp_single_palette)
+    expect_doppelganger("single palette plot", disp_single_palette)
     
     disp_multiple_palettes <- function() plot(clpalettes(set = "top", 
                                                          lover = "Skyblue2u", 
                                                          numResults = 1), 
                                               ask = FALSE)
-    vdiffr::expect_doppelganger("multiple palette plots", disp_multiple_palettes)
+    expect_doppelganger("multiple palette plots", disp_multiple_palettes)
     
     disp_pie_palette <- function()  plot(clpalette("4643792"), type = "pie")
-    vdiffr::expect_doppelganger("single palette pie chart", disp_pie_palette)
+    expect_doppelganger("single palette pie chart", disp_pie_palette)
     
     disp_multiple_pie_palettes <- function() plot(clpalettes(set = "top", 
                                                              lover = "Skyblue2u", 
                                                              numResults = 1), 
                                                   ask = FALSE, pie = TRUE)
-    vdiffr::expect_doppelganger("multiple palette pie charts", disp_multiple_pie_palettes)
+    expect_doppelganger("multiple palette pie charts", disp_multiple_pie_palettes)
     
     disp_small_palette <- function() plot(clpalette("4643793"))
-    vdiffr::expect_doppelganger("single small palette plot", disp_small_palette)
+    expect_doppelganger("single small palette plot", disp_small_palette)
   })
 })
 
@@ -58,21 +58,21 @@ with_mock_api({
     testthat::skip_on_cran()
     
     disp_single_pattern <- function() plot(clpattern("5794648"))
-    vdiffr::expect_doppelganger("single pattern plot", disp_single_pattern)
+    expect_doppelganger("single pattern plot", disp_single_pattern)
     
     disp_multiple_patterns <- function() plot(clpatterns(set = "top", 
                                                          lover = "Skyblue2u", 
                                                          numResults = 1), 
                                               ask = FALSE)
-    vdiffr::expect_doppelganger("multiple pattern plots", disp_multiple_patterns)
+    expect_doppelganger("multiple pattern plots", disp_multiple_patterns)
     
     disp_pie_pattern <- function()  plot(clpattern("5794648"), type = "pie")
-    vdiffr::expect_doppelganger("single pattern pie chart", disp_pie_pattern)
+    expect_doppelganger("single pattern pie chart", disp_pie_pattern)
     
     disp_multiple_pie_patterns <- function() plot(clpatterns(set = "top", 
                                                              lover = "Skyblue2u", 
                                                              numResults = 1), 
                                                   ask = FALSE, pie = TRUE)
-    vdiffr::expect_doppelganger("multiple pattern pie charts", disp_multiple_pie_patterns)
+    expect_doppelganger("multiple pattern pie charts", disp_multiple_pie_patterns)
   })
 })
